01226db964e9c6c3741e45b74760168c6bc5467c
richard miruka onsare <richardonsare15@gmail.com>
2023-04-05 11:41:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    const messagesArray = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+      messagesArray.push(message);
+    }
+  
+    return messagesArray;
+  }
+  
+  function countDown(number) {
+    let i = number;
+  
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
